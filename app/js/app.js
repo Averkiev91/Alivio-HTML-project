6c5b8fc588919aad0f035ac12252881ae5840037
Averkiev91 <amd3500m@gmail.com>
2021-11-19 18:27:30 +0300

    hamburger

--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,13 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+    const mobileButton = document.querySelector('.mobile__button')
+    const mobileNavIcon = document.querySelector('.mobile__button-icon')
+    const mobileOver = document.querySelector('.mobile__overlay')
+
+    mobileButton.addEventListener('click', () => {
+        mobileNavIcon.classList.toggle('active')
+        mobileOver.classList.toggle('active')
+        document.body.classList.toggle('no-scroll')
+    })
 })
