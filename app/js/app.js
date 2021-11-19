b83ee4b5329bd7569005fbe885975f8fbb995700
Averkiev91 <amd3500m@gmail.com>
2021-11-19 23:19:13 +0300

    play js

--- app/js/app.js
+++ app/js/app.js
@@ -11,4 +11,38 @@ document.addEventListener('DOMContentLoaded', () => {
         mobileOver.classList.toggle('active')
         document.body.classList.toggle('no-scroll')
     })
+
+
+    const videoBtn = document.querySelector('.story__video-svg')
+    const videoObj = document.querySelector('.story__video-obj')
+    const overlay = document.querySelector('.story__video-overlay')
+
+    videoBtn.addEventListener('click', () => {
+
+        function toggleOverlay(event) {
+            if (event.type === 'mouseleave') {
+                overlay.classList.add('hidden');
+            } else {
+                overlay.classList.remove('hidden');
+            }
+        }
+
+        if (videoObj.paused) {
+            videoObj.play()
+            videoBtn.innerHTML = `<svg class="story__video-svg">
+                                <use xlink:href="#pause-white"></use>
+                            </svg>`
+
+            overlay.onmouseleave = toggleOverlay
+            overlay.onmouseenter = toggleOverlay
+        } else {
+            videoObj.pause()
+            videoBtn.innerHTML = `<svg class="story__video-svg">
+                                <use id="svg" xlink:href="#play-white"></use>
+                            </svg>`
+
+            overlay.onmouseleave = null
+            overlay.onmouseleave = null
+        }
+    })
 })
